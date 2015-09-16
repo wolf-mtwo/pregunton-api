416e95e00fd5768a0feb3be7f90e7cf282a29e77
wolf-mtwo <wolf.mtwo@outlook.com>
2015-09-15 23:46:07 -0400

    Updated configuration files

--- config/user.s
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(25) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
